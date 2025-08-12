46c8964eada787998db129e17182f7225b198b3c
Study.click <studyclik@gmail.com>
2025-08-12 18:08:58 -0300

    primeiro commit logo

---
+++ cadastro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de cadastro</title>
+    <link rel="stylesheet" href="estilo.css" />
+</head>
+<body>
+    
+</body>
+</html>
+
+<div class="login-card">
+      <h1>Criar nova conta</h1>
+      <p class="subtext">Já está cadastrado? Faça login <a href="login.php">aqui</a>.</p>
+
+      <form class="form">
+        <label for="nome">Nome</label>
+        <input type="text" id="nome" placeholder="Jiara Martins" />
+       
+        <label for="data">Data de nascimento</label>
+        <input type="text" id="data" placeholder="00/00/0000" />
+
+
+        <label for="email">Email</label>
+        <input type="text" id="email" placeholder="hello@gmail.com" />
+
+        <label for="senha">Senha</label>
+        <input type="password" id="senha" placeholder="********" />
+
+       
+        <button type="submit" class="btn">Cadastrar-se</button>
+      </form>
+    </div>
---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="estilo.css" />
+  <title>Login - Study Click</title>
+  
+</head>
+<body>
+  <div class="login-card">
+    <p> "A língua é o nosso primeiro universo"</p>
+    <img src="logo2.png" alt="Logo do Study Click">
+    <h1>Login</h1>
+    <input type="text" placeholder="Nome">
+    <input type="password" placeholder="Senha">
+    <button>Entrar</button>
+    
+      <div>
+      <a href="cadastro.html"> Cadastrar-se</a>
+      
+  </div>
+</body>
+</html>
